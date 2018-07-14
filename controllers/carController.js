505ea157e0886660610f1ec2a54c4cdc06abc7ae
Badal Moradia <badal@i360llc.com>
2018-07-14 15:43:44 -0500

    created index page

--- controllers/carController.js
+++ controllers/carController.js
@@ -29,6 +29,8 @@ router.post('/', (req, res) => {
 			console.log(newCar);
 			res.redirect('/cars');
 		}
+	})
+})
 
 
 //NEW ROUTE
@@ -96,26 +98,7 @@ router.delete('/:id', (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = router;
 
 
 
